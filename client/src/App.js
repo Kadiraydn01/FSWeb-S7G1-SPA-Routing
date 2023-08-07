fce5874980457e068614dbea47a7620fd40d262d
Kadiraydn01 <kadiraydn01@gmail.com>
2023-08-07 19:18:07 +0300

    İlk Başlangıç Yapıldı

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Film from "./Filmler/Film";
+import KaydedilenlerListesi from "./Filmler/KaydedilenlerListesi";
 
-import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
-
-export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+export default function App() {
+  const [saved, setSaved] = useState([]);
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const FilmleriAl = () => {
       axios
-        .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
-        .then(response => {
-          // Bu kısmı log statementlarıyla çalışın
-          // ve burdan gelen response'u 'movieList' e aktarın
+        .get("http://localhost:5001/api/filmler")
+        .then((response) => {
+          setSaved(response.data);
+          setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Sunucu Hatası', error);
+        .catch((error) => {
+          console.error("Sunucu Hatası", error);
         });
-    }
+    };
+
     FilmleriAl();
   }, []);
 
-  const KaydedilenlerListesineEkle = id => {
-    // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
+  console.log(saved);
+
+  const KaydedilenlerListesineEkle = (id) => {
+    const kaydedilmis = saved.find((film) => film.id === id);
+    if (kaydedilmis) {
+      console.log("Bu filmi zaten kaydettiniz!");
+    } else {
+      const secili = movieList.find((film) => film.id === id);
+      if (secili) {
+        setSaved([...saved, secili]);
+      }
+    }
   };
 
   return (
     <div>
-      <KaydedilenlerListesi list={[ /* Burası esnek */]} />
+      <Film id={1} />
+      <KaydedilenlerListesi
+        list={saved}
+        //   [
+        //     /* Burası esnek */
+        //   ]
+        // }
+      />
 
-      <div>Bu Div'i kendi Routelarınızla değiştirin</div>
+      <div>Routerlar buraya</div>
     </div>
   );
 }
